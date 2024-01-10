8e85b8dd8c17b65b7c145035daf233e7cdda58e9
AithaNishanth <117250403+AithaNishanth@users.noreply.github.com>
2024-01-10 16:34:16 +0530

    update
    update

--- src/app/shared/headerbuttons/headerbuttons.component.ts
+++ src/app/shared/headerbuttons/headerbuttons.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit  } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { IonModal } from '@ionic/angular';
+import { CommonService } from 'src/app/services/common.service';
+import { LoaderService } from 'src/app/services/loader.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-headerbuttons',
@@ -10,6 +13,22 @@ import { IonModal } from '@ionic/angular';
 export class HeaderbuttonsComponent  implements OnInit {
   isModalOpen = false;
   isFilterModalOpen=false;
+  locationColumns: any = [
+    "id",
+    "location_name",
+    "location_anpr",
+    "status",
+    "created_on"
+  ];
+  locationOrder: any={
+    "location_name": "asc"
+  };
+  locationFilter: any = {
+    
+  }
+  userData: any;
+  locationData: any;
+  selectedLocId: any;
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
@@ -19,9 +38,43 @@ setFilterOpen(isOpen: boolean){
 this.isFilterModalOpen = isOpen;
   }
 
-  constructor() { }
+  constructor(
+    private loader: LoaderService,
+    private common: CommonService,
+    private activatedRouterService: ActivatedRoute,
+    private router: Router
+  ) {
+    let userInfo:any = localStorage.getItem('userData')
+    // console.log(userInfo);
+    this.userData = JSON.parse(userInfo)
+    console.log(this.userData); 
+   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.locationDetails();
+    
+  }
+
+   onLocationChange(event: any) {
+    this.selectedLocId = event.detail.value;
+    console.log(this.selectedLocId);
+    
+  }
+  async locationDetails(){
+    // await this.loader.showLoader();
+
+    let payload ={
+      "columns":this.locationColumns,
+      "order_by": this.locationOrder,
+      "filters":this.locationFilter
+    }
+    console.log("payload",payload);
+    this.common.getLocations(payload).subscribe((resp: any)=>{
+      console.log(resp.data);
+      this.locationData = resp.data;
+      console.log(this.locationData);
+    })
+  }
 
 }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   apiUrl: "https://rpwebapps.us/clients/sandsale/api/v1/",
   imgUrl:"",
+  authorizationURL:"rpwebapps.us:8087",
   production: false
 };
 
